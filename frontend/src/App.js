7f25caeee6983e3f1fc219f398dd7202bab525e7
Aparna Shree <aparnashreegopi@gmail.com>
2025-07-14 20:33:53 -0400

    creating a frontend folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Camera } from 'lucide-react';
 import './index.css'; 
 
-const API_URL = 'http://localhost:8000';
-
 function App() {
+
+  const API_URL = process.env.API_URL;
   const [selectedFile, setSelectedFile] = useState(null);
   const [preview, setPreview] = useState(null);
   const [results, setResults] = useState([]);
